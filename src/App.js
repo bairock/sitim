e499811c8ac5cc18e8758ef0626e5ce43b737c59
bairock <bairock@live.ru>
2022-11-22 15:05:33 +0600

    mobile menu

--- src/App.js
+++ src/App.js
@@ -37,7 +37,6 @@ const App = () => {
     }
   }
 
-
   return (
     <View>
       <Header onScrollTo={onScrollTo} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native'
 import { useMediaQuery } from 'react-responsive'
+import { HiMenu, HiX } from "react-icons/hi"
 import logo from '../assets/logo.svg'
 import documents from '../assets/docs.zip'
 
 const Header = ({ onScrollTo }) => {
+    const [menu, setMenu] = useState(false)
     const is1115 = useMediaQuery({ query: '(max-width: 1115px)' })
+
+    useEffect(() => {
+        if (!is1115) setMenu(false)
+    }, [is1115, menu])
 
     return (
         <View style={styles.container}>
@@ -29,9 +36,36 @@ const Header = ({ onScrollTo }) => {
                                 <Text style={styles.menuText}>Документы</Text>
                             </TouchableOpacity>
                         </View>
-                    ) : null
+                    ) : (
+                        <TouchableOpacity onPress={() => setMenu(prev => !prev)}>
+                            {
+                                !menu ? <HiMenu size={50} color='#fff' /> : <HiX size={50} color='#fff' />
+                            }
+                        </TouchableOpacity>
+                    )
                 }
             </View>
+            {
+                menu ? (
+                    <View style={styles.mobileMenu}>
+                        <TouchableOpacity onPress={() => onScrollTo('network')}>
+                            <Text style={styles.mobileMenuText}>Сеть пространств</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => onScrollTo('about')}>
+                            <Text style={styles.mobileMenuText}>О пространстве</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => onScrollTo('open')}>
+                            <Text style={styles.mobileMenuText}>Как открыть пространство</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => onScrollTo('team')}>
+                            <Text style={styles.mobileMenuText}>Команда развития сети</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity accessibilityRole='link' href={documents} download={'Документы'}>
+                            <Text style={styles.mobileMenuText}>Документы</Text>
+                        </TouchableOpacity>
+                    </View>
+                ) : null
+            }
         </View>
     )
 }
@@ -62,6 +96,20 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         width: '100%',
         paddingHorizontal: 24
+    },
+    mobileMenu: {
+        maxWidth: 1366,
+        alignSelf: 'center',
+        width: '100%',
+        paddingHorizontal: 24,
+        paddingBottom: 24
+    },
+    mobileMenuText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: '500',
+        marginHorizontal: 12,
+        marginVertical: 12
     }
 })
 
