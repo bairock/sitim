8e1e8f2dd9abf9606227726e059fe04cc52e373a
bairock <bairock@live.ru>
2022-12-15 16:35:42 +0900

    125125

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Open from './components/Open'
 import Team from './components/Team'
 
 const client = new ApolloClient({
-  uri: 'graphql',
+  uri: 'https://admin.sitim.site/graphql',
   cache: new InMemoryCache(),
 })
 
